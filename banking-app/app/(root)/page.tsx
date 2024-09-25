eee8709ed26b447940ad1232b16a7045e9e379c1
koromz <alimkoroma@outlook.com>
2024-09-25 16:37:32 +0100

    Header Component added to Home Page

--- banking-app/app/(root)/page.tsx
+++ banking-app/app/(root)/page.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import HeaderBox from '@/components/ui/HeaderBox'
 
 function Home() {
+  const loggedIn = {firstname: 'Alim'}
   return (
-    <div>
-      Home
-    </div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+        <HeaderBox
+        type="greeting"
+        title="Welcome"
+        user={loggedIn?.firstname || 'Guest'}
+        subtext = "Access and manage your account and transactions"
+        />
+        </header>
+      </div>
+    </section>
   )
 }
 
