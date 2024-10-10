d527b7e46e90a06ee91275969a8f9ecb2d6246bb
koromz <alimkoroma@outlook.com>
2024-10-10 17:55:11 +0100

    Added Mobile Nav

--- banking-app/app/(root)/layout.tsx
+++ banking-app/app/(root)/layout.tsx
@@ -1,4 +1,6 @@
 import Sidebar from "@/components/ui/Sidebar"
+import Image from 'next/image'
+import MobileNav from "@/components/ui/MobileNav";
 export default function RootLayout({
   children,
 }: Readonly<{
@@ -9,7 +11,19 @@ export default function RootLayout({
   return (
    <main className="flex h-screen w-full font-inter"> 
     <Sidebar user={loggedIn}/>
-    {children}
+    <div className ="flex size-full flex-col">
+      <div className="root-layout">
+      <Image src="/icons/logo.svg" width={30}
+      height={30}
+      alt ="logo"
+      />
+      <div>
+        <MobileNav user={loggedIn}/>
+      </div>
+      </div>
+      {children}
+    </div>
+    
    </main>
   );
 }
